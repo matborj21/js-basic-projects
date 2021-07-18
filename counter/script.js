4cd53d2d8b8795fb1ab3b2a891511e85a9be2e01
Omat borje <omatborje@gmail.com>
2021-07-18 13:25:06 +0800

    done with the basic counter

--- counter/script.js
+++ counter/script.js
@@ -1,1 +1,45 @@
-console.log("Hello World");
+const btnDown = document.querySelector('#btn-down');
+const btnReset = document.querySelector('#btn-reset');
+const btnUp = document.querySelector('#btn-up');
+let counter = document.getElementById('counter');
+
+
+const addColorBlue = () => {
+    if (counter.textContent == 0) {
+        return counter.style.color = "#1d1c1c";
+    }
+    if (counter.textContent > 0) {
+        return counter.style.color = "blue";
+    }
+}
+
+const addColorRed = () => {
+    if (counter.textContent == 0) {
+        return counter.style.color = "#1d1c1c";
+    }
+    if (counter.textContent < 0) {
+        return counter.style.color = "red";
+    }
+}
+const add = () => {
+    let addition = counter.textContent = parseInt(counter.textContent) + 1;
+    addColorBlue();
+    return addition;
+}
+
+const reset = () => {
+    counter.textContent = 0;
+    counter.style.color = '#1d1c1c';
+}
+
+const subtract = () => {
+    let substraction = counter.textContent = parseInt(counter.textContent) - 1;
+    addColorRed();
+    return substraction;
+}
+
+// btnDown.addEventListener('click', subtract);
+btnReset.addEventListener('click', reset);
+// btnUp.addEventListener('click', add);
+btnUp.addEventListener('mousedown', add);
+btnDown.addEventListener('mousedown', subtract)
